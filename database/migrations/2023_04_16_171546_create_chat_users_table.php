42963db30116b7198c637dfb1161db5c95d6a989
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-17 14:37:43 -0400

    changing names of table

--- database/migrations/2023_04_16_171546_create_chat_users_table.php
+++ database/migrations/2023_04_16_171546_create_chat_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('chat_users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('firstName');
             $table->string('lastName');
--- database/migrations/2023_04_16_173454_create_chat_messages_table.php
+++ database/migrations/2023_04_16_173454_create_chat_messages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('message', function (Blueprint $table) {
+        Schema::create('chat_messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('description');
             $table->binary('image')->nullable();
