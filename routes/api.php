85dc09622d245b099d27d96307a65796424d8c5a
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-25 01:15:08 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::post('/users/login', [PageController::class, 'userLogin']);
 
 
 Route::get('/messages', [PageController::class, 'getMessages']);
+Route::get('/messages/{id}', [PageController::class, 'getMessagesById']);
 Route::get('/messages/users/{user_one_id}&&{user_two_id}', [PageController::class, 'getRecentMessages']);
 Route::post('/messages', [PageController::class, 'postMessage']);
 
